ea17f2b1671e931eae2dacb6ec9cf30709513d5e
PonyLucky <60187395+PonyLucky@users.noreply.github.com>
2021-09-22 22:36:56 +0200

    Init pos of 2/3 routines are done

--- Routine.py
+++ Routine.py
@@ -11,3 +11,29 @@ class Routine():
     def __del__(self):
         del self.lisPos
         del self.posInit
+
+    def shaker(self):
+        self.posInit = Position(
+            {
+                'm1': -90,
+                'm2': 0,
+                'm3': 0,
+                'm4': 180,
+                'm5': 90,
+            },
+            5,
+            5
+        )
+    
+    def essuieGlace(self):
+        self.posInit = Position(
+            {
+                'm1': 0,
+                'm2': 90,
+                'm3': 0,
+                'm4': 0,
+                'm5': -90,
+            },
+            5,
+            5
+        )
